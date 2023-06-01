f1ec0e95bd4650349783ebe139d5440366c83858
Julia <julia@Julias-Air.attlocal.net>
2023-06-01 19:51:32 -0400

    added my mod

--- Addition.java
+++ Addition.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Addition {
     public static void main(String []args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("How many numbers do you want to add together?");
         int numbers = sc.nextInt();
         int sum = 0;
 
-        for (int i = 20; i <= numbers; ++i) {
+        for (int i = 1; i <= numbers; ++i) {
             System.out.println("Enter the " + convert_to_ordinal(i) + " number.");
             int number = sc.nextInt();
             sum = sum + number;
@@ -23,23 +23,28 @@ public class Addition {
     
     public static String convert_to_ordinal(int number) {
         int mod = 0;
-        if (number == 1) {
-            return "1st";
+        Mod modulus = new Mod();
+
+
+        mod = modulus.mod(number, 10);
+      
+        if (mod == 1) {
+            return number + "st";
         }
-        if (number == 2) {
-            return "2nd";
+        if (mod == 2) {
+            return number + "nd";
         }
-        if (number == 3) {
-            return "3rd";
+        if (mod == 3) {
+            return number + "rd";
         }
-        if (number >= 4 && number <= 20) {
+        if ((number >= 4 && number <= 20) || is_4to9and0(mod) ) {
             return number + "th";
         }
-        if (number > 20) {
-            mod = number % 10;
-        }
+       return "Oh oh, unexpected condition!"; 
 
-        return "";
+    }
+    public static Boolean is_4to9and0(int mod) {
+        return (mod == 4 || mod == 5 || mod == 6 || mod == 7 || mod == 8 || mod == 9 || mod == 0 );
     }
 }
 
